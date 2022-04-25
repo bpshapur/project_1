507317b71591cc9465c667ca695ca0793ed52dd5
Sukumar Suryawanshi <Sukumar.Suryawanshi@ge.com>
2022-04-25 07:13:11 +0530

    Mon, Apr 25, 2022  7:13:11 AM

--- santosh/buildec2.sh
+++ santosh/buildec2.sh
@@ -1,5 +1,7 @@
-aws ec2  run-instances \
-    --image-id ami-0e472ba40eb589f49 \
-    --instance-type t2.micro \
-    --key-name dev_workstation_1 \
-    --region us-east-1
+/ aws ec2  run-instances \
+/    --image-id ami-0e472ba40eb589f49 \
+/    --instance-type t2.micro \
+/    --key-name dev_workstation_1 \
+/    --region us-east-1
+
+aws ec2 descibe-instances
