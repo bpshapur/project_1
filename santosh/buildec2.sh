c4c5ad9d583ad421940d7156931146c564a7ea3a
Sukumar Suryawanshi <Sukumar.Suryawanshi@ge.com>
2022-04-26 07:06:50 +0530

    added ls pwd and buildsh

--- santosh/buildec2.sh
+++ santosh/buildec2.sh
@@ -1,7 +1,5 @@
-/ aws ec2  run-instances \
-/    --image-id ami-0e472ba40eb589f49 \
-/    --instance-type t2.micro \
-/    --key-name dev_workstation_1 \
-/    --region us-east-1
-
-aws ec2 describe-instances
+aws ec2  run-instances \
+     --image-id ami-0e472ba40eb589f49 \
+     --instance-type t2.micro \
+     --key-name dev_workstation_1 \
+     --region us-east-1
