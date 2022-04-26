de2751bc5e3655b417bf6aeb7f6b1bdf3717168e
bpshapur <basavaprabhu.shapur@gmail.com>
2022-04-26 07:28:19 +0530

    Tue Apr 26 07:28:18 IST 2022

--- santosh/buildec2.sh
+++ santosh/buildec2.sh
@@ -2,5 +2,5 @@ aws ec2  run-instances \
      --image-id ami-0e472ba40eb589f49 \
      --instance-type t2.micro \
      --key-name dev_workstation_1 \
-     --region us-east-1
-     --tag-specifications=ResourceType=instance,Tags=[{Key=Name,Value=demo-jenkins-1}]
+     --region us-east-1 \
+     --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value=demo-jenkins-1}]
