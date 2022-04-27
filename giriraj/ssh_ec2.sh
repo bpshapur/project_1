4857a77f9fdd623ccb50c847d917dd8e66460810
Sukumar Suryawanshi <Sukumar.Suryawanshi@ge.com>
2022-04-27 08:16:21 +0530

    added -o StrictHostKeyChecking no

--- giriraj/ssh_ec2.sh
+++ giriraj/ssh_ec2.sh
@@ -1,3 +1,3 @@
 # cd project_1/giriraj && \
-chmod 400 dev_workstation_2.pem && \
-ssh -o "StrictHostKeyChecking no" -i "dev_workstation_2.pem" ec2-user@ec2-3-92-189-23.compute-1.amazonaws.com
+chmod 400 dev_workstation_2.pem
+ssh -o "StrictHostKeyChecking no" -i "dev_workstation_2.pem" ubuntu@ec2-44-200-126-221.compute-1.amazonaws.com
