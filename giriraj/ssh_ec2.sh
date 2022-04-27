823939494a992b6ae28fefca28634153d925c98a
Sukumar Suryawanshi <Sukumar.Suryawanshi@ge.com>
2022-04-27 08:05:13 +0530

    update jenkins with ssh_ec2

--- giriraj/ssh_ec2.sh
+++ giriraj/ssh_ec2.sh
@@ -1,4 +1,3 @@
-git clone -b dev https://github.com/bpshapur/project_1.git && \
-cd project_1/giriraj && \
+# cd project_1/giriraj && \
 chmod 400 dev_workstation_2.pem && \
 ssh -i "dev_workstation_2.pem" ec2-user@ec2-3-92-189-23.compute-1.amazonaws.com
