5c94dd55c762e2662a534bf26efff0ef27ed8dc3
Sukumar Suryawanshi <Sukumar.Suryawanshi@ge.com>
2022-04-27 08:17:51 +0530

    added mkdir for ec2

--- giriraj/ssh_ec2.sh
+++ giriraj/ssh_ec2.sh
@@ -1,3 +1,4 @@
 # cd project_1/giriraj && \
 chmod 400 dev_workstation_2.pem
-ssh -o "StrictHostKeyChecking no" -i "dev_workstation_2.pem" ubuntu@ec2-44-200-126-221.compute-1.amazonaws.com
+ssh -o "StrictHostKeyChecking no" -i "dev_workstation_2.pem" ubuntu@ec2-44-200-126-221.compute-1.amazonaws.com && \
+mkdir -p /tmp/sukumar
